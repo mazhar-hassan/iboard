35943a49d70bd41d5cb24c94764b5893b20d00d1
sakhi <sakhi.mansoor@venturedive.com>
2018-09-11 01:25:38 +0500

    bootstrap 4 pre reqs added

--- webpack.config.js
+++ webpack.config.js
@@ -3,25 +3,25 @@ console.log(__dirname);
 module.exports = {
     entry: "./src/app.js",
     output: {
-        path:path.join(__dirname,"public"),
-        filename:"bundle.js"
+        path: path.join(__dirname, "public"),
+        filename: "bundle.js"
     },
     module: {
-        rules:[{
-            loader:"babel-loader",
-            test:/\.js$/,
-            exclude:/node_modules/
+        rules: [{
+            loader: "babel-loader",
+            test: /\.js$/,
+            exclude: /node_modules/
         }, {
-            test:/\.scss$/,
+            test: /\.(css|scss)$/,
             use: [
                 'style-loader',
                 'css-loader',
                 'sass-loader'
-              ]
+            ]
         }]
     },
-    devtool:'cheap-module-eval-source-map',
+    devtool: 'cheap-module-eval-source-map',
     devServer: {
-        contentBase : path.join(__dirname,"public")
+        contentBase: path.join(__dirname, "public")
     }
 }
